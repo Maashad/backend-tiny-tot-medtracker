05d1a74ea787fe844789d50a2b947e1dcc607431
Amber Shay <mashaad21@gmail.com>
2023-08-07 15:12:52 -0600

    deleted unique name requirement

--- app/models/child_model.py
+++ app/models/child_model.py
@@ -2,6 +2,6 @@ from ..extensions import db
 
 class Child(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
+    name = db.Column(db.String(100))
 
     medications = db.relationship("Medication", back_populates="children")
