63df497026ff6985814ee486b4b7b9d08b497723
Amber Shay <mashaad21@gmail.com>
2023-08-07 16:31:38 -0600

    switched db to postgresql

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 from .extensions import api, db
 from .routes.child_routes import ns_child
 from .routes.med_routes import ns_med
@@ -6,13 +7,17 @@ from .routes.med_routes import ns_med
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.tinytot"
+    migrate = Migrate(app, db)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/tinytot'
 
     from app.models.medication_model import Medication
     from app.models.child_model import Child
 
     api.init_app(app)
     db.init_app(app)
+    migrate.init_app(app, db)
 
     api.add_namespace(ns_child)
     api.add_namespace(ns_med)
